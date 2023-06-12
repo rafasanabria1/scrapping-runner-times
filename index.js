39fefdcb1572f8b9cb48f058086582bf777b7b9e
Rafael Sanabria Cruz <rafasanabria1@gmail.com>
2023-06-13 00:42:09 +0200

    Construir array de bbdd con el scrapping de cada carrera

--- index.js
+++ index.js
@@ -1,11 +1,20 @@
 // @ts-check
 const { chromium } = require('playwright')
+const bd = { races: [], cities: new Set() }
 
-;(async () => {
+const saveRace = ({ name, city, link, date }) => {
+  bd.races.push({ name, city, link, date })
+}
+
+const saveCity = ({ city }) => {
+  bd.cities.add(city)
+}
+
+const scrapHome = async () => {
   const browser = await chromium.launch({ headless: true })
+
   const page = await browser.newPage()
   await page.goto('https://www.ideain.com/')
-
   const links = await page.$$eval('a', links => {
     return links.map(link => link.href).filter(link => {
       const months = ['enero', 'febrero', 'marzo', 'abril', 'mayo', 'junio', 'julio', 'agosto', 'septiembre', 'octubre', 'noviembre', 'diciembre']
@@ -13,6 +22,7 @@ const { chromium } = require('playwright')
       return dateRaw && dateRaw[1] === dateRaw[3] && months.includes(dateRaw[1]) && dateRaw[2] === dateRaw[4]
     })
   })
+  await page.close()
 
   for (const link of links) {
     const newPage = await browser.newPage()
@@ -20,18 +30,46 @@ const { chromium } = require('playwright')
 
     const rows = await newPage.$$('tbody > tr')
 
-    console.log({ filas: rows.length })
-
-    // eslint-disable-next-line no-unreachable-loop
     for (const row of rows) {
       const data = await row.$$('td > p font')
+      if (data.length !== 3) continue
 
-      console.log({ link, elementosFila: data.length })
+      const date = await data[0].textContent().then(date => {
+        if (!date) return null
+        const [day, month, year] = date?.trim().split('-')
+        return new Date(parseInt('20' + year), parseInt(month), parseInt(day))
+      })
+      const city = await data[1].textContent().then(city => city?.trim().replace(/\n\s+/, ' '))
+
+      const { name, link } = await data[2].$eval('a', anchor => {
+        return { name: anchor.innerHTML.trim().replace(/\n\s+/, ' '), link: anchor.href }
+      })
+
+      saveRace({ name, city, link, date })
+      saveCity({ city })
     }
 
-    newPage.close()
+    await newPage.close()
   }
 
-  page.close()
   await browser.close()
+}
+
+const scrapRaces = async () => {
+  const browser = await chromium.launch({ headless: true })
+  const page = await browser.newPage()
+  for (const race of bd.races) {
+    await page.goto(race.link)
+    const $pre = await page.$('pre')
+    const txt = await $pre?.textContent().then(txt => txt.split('\n'))
+
+    page.close()
+    browser.close()
+    return
+  }
+}
+
+(async () => {
+  await scrapHome()
+  await scrapRaces()
 })()
